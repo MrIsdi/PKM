7203c2f7f2c1fdd17a7e97521b0174efca322b53
MrIsdi <isdiridho@gmail.com>
2023-10-11 16:01:43 +0700

    Update

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -11,7 +11,7 @@ function Research() {
                 <div className='flex mt-8 justify-between lg:gap-x-8 lg:flex-row flex-col-reverse'>
                     <div className="lg:basis-2/3 border border-[rgba(255, 255, 255, 0.62)] bg-[#2d2d2d73] backdrop-blur-[10px] rounded-[20px] p-10 text-justify flex items-center">
                         <p className='text-xl text-white'>
-                            Sistem pemantauan perkembangan bibit tanaman tomat berbasis IoT memiliki parameter pengukur yang dilakukan. Parameter tersebut diantaranya: suhu, kelembapan udara, intensitas cahaya dan tumbuh/belum tumbuh bibit tanaman tomat. Pengukuran parameter suhu dan kelembapan udara diukur oleh sensor DHT11. Pengukuran parameter intensitas cahaya diukur oleh sensor LDR. Dan pengukuran parameter tumbuh/ belum tumbuh diukur oleh kamera yang diproses menggunakan algoritma YOLOV8.
+                            Sistem pemantauan dan pengendalian bibit tanaman tomat berbasis IoT memiliki parameter pengukur yang dilakukan. Parameter tersebut diantaranya: suhu, kelembapan udara, intensitas cahaya dan tumbuh/belum tumbuh bibit tanaman tomat. Pengukuran parameter suhu dan kelembapan udara diukur oleh sensor DHT11. Pengukuran parameter intensitas cahaya diukur oleh sensor LDR. Dan pengukuran parameter tumbuh/ belum tumbuh diukur oleh kamera yang diproses menggunakan algoritma YOLOV8.
                         </p>
                     </div>
                     <div className="lg:basis-1/3 border border-[rgba(255, 255, 255, 0.62)] bg-[#2d2d2d73] backdrop-blur-[10px] rounded-[20px] p-5 flex items-center">
